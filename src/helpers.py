dcc496ec610d1471d1f86704f1bd6b9007a0695b
TrucDev <jackyhuynh87@gmail.com>
2022-02-02 15:04:51 -0500

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,7 @@
 
 import networkx as nx
 import pickle
-import plotly.plotly as py
 import random
-from plotly.graph_objs import *
 from plotly.offline import init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 
